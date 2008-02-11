c044bccf8c1b0b83010c56fcd75b1a3c21b3a7be
Jim Huang <jserv.tw@gmail.com>
2008-02-11 16:15:11 +0000

    Build time selecting ALSA/OSS volume plugin. TODO: real OSS checking.

--- src/plugins/volumealsa/volumealsa.c
+++ src/plugins/volumealsa/volumealsa.c
@@ -1,5 +1,5 @@
-/**                                                                                                 
- * Copyright (c) 2006 LxDE Developers, see the file AUTHORS for details.
+/**
+ * Copyright (c) 2008 LxDE Developers, see the file AUTHORS for details.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
