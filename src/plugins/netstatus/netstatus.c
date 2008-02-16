78dc5880dca2615f2f351f7731f1f6fd78884b57
Fred Chien <fred@lxde.org>
2008-02-16 22:14:38 +0000

    Fix confused background of dialog window in netstatus plugin.

--- src/plugins/netstatus/netstatus.c
+++ src/plugins/netstatus/netstatus.c
@@ -75,6 +75,10 @@ static void on_button_press( GtkWidget* widget, GdkEventButton* evt, plugin* p )
         {
             iface = netstatus_icon_get_iface( NETSTATUS_ICON(widget) );
             ns->dlg = netstatus_dialog_new(iface);
+
+            /* fix background */
+            gtk_widget_set_style(ns->dlg, p->panel->defstyle);
+
             netstatus_dialog_set_configuration_tool( ns->dlg, ns->config_tool );
             g_signal_connect( ns->dlg, "response", G_CALLBACK(on_response), ns );
         }
