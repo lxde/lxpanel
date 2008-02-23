1934880234296c1eb30af9448889a292d0735d1a
Fred Chien <fred@lxde.org>
2008-02-23 19:04:42 +0000

    Add netstat setting and icons to configure.ac.

--- src/plugins/netstat/nsconfig.h
+++ src/plugins/netstat/nsconfig.h
@@ -2,15 +2,15 @@
 #define HAVE_NSCONFIG_H
 
 #define RESOURCE_DIR "/usr/share/fnetdaemon"
-#define ICONS_CONNECTED		RESOURCE_DIR "/connected.png"
-#define ICONS_DISCONNECT	RESOURCE_DIR "/disconnect.png"
-#define ICONS_PROBLEM		RESOURCE_DIR "/problem.png"
-#define ICONS_RENEW		RESOURCE_DIR "/renew.png"
-#define ICONS_BOTHRS		RESOURCE_DIR "/bothrs.png"
-#define ICONS_SENDDATA		RESOURCE_DIR "/senddata.png"
-#define ICONS_RECVDATA		RESOURCE_DIR "/recvdata.png"
-#define ICONS_WL_CONNECTED	RESOURCE_DIR "/wlconnected.png"
-#define ICONS_WL_DISCONNECT	RESOURCE_DIR "/wldisconnect.png"
-#define ICONS_WL_RENEW		RESOURCE_DIR "/wlrenew.png"
+#define ICONS_CONNECTED		RESOURCE_DIR "/ns-connected.png"
+#define ICONS_DISCONNECT	RESOURCE_DIR "/ns-disconnect.png"
+#define ICONS_PROBLEM		RESOURCE_DIR "/ns-problem.png"
+#define ICONS_RENEW		RESOURCE_DIR "/ns-renew.png"
+#define ICONS_BOTHRS		RESOURCE_DIR "/ns-bothrs.png"
+#define ICONS_SENDDATA		RESOURCE_DIR "/ns-senddata.png"
+#define ICONS_RECVDATA		RESOURCE_DIR "/ns-recvdata.png"
+#define ICONS_WL_CONNECTED	RESOURCE_DIR "/ns-wlconnected.png"
+#define ICONS_WL_DISCONNECT	RESOURCE_DIR "/ns-wldisconnect.png"
+#define ICONS_WL_RENEW		RESOURCE_DIR "/ns-wlrenew.png"
 
 #endif
