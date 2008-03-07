c3074ccc5ba61c85c878edab7989aab33bcadb68
Jim Huang <jserv.tw@gmail.com>
2008-03-07 10:16:00 +0000

    trivial indent.

--- src/plugins/netstat/wireless.c
+++ src/plugins/netstat/wireless.c
@@ -1,4 +1,4 @@
-/**																								 
+/**
  * Copyright (c) 2008 LxDE Developers, see the file AUTHORS for details.
  *
  * This program is free software; you can redistribute it and/or modify
