cb99090b9912ec5cd6c21b6fd45fcc3bd0c4b5de
Fred Chien <fred@lxde.org>
2008-05-05 19:09:44 +0000

    * fixed BUG that cancel wifi password dialog cause panel crash. * the AP that is hidden with encryption will be remove from scanning list * improving IE information handler to support WPA/WPA2 Encryption. * more and more stuff for new LXNM, please upgrade LXNM to lastest version in svn.

--- src/plugins/netstat/wireless.h
+++ src/plugins/netstat/wireless.h
@@ -3,15 +3,32 @@
 
 #include "netstat.h"
 
-#define NS_WIRELESS_AUTH_OFF       0
-#define NS_WIRELESS_AUTH_WEP       1
-#define NS_WIRELESS_AUTH_WPA_PSK   2
+/* auth protocol */
+#define NS_WIRELESS_AUTH_OFF          0
+#define NS_WIRELESS_AUTH_WEP          1
+#define NS_WIRELESS_AUTH_WPA          2
+
+/* Cypher values in GENIE (pairwise and group) */
+#define NS_IW_IE_CIPHER_NONE          0
+#define NS_IW_IE_CIPHER_WEP40         1
+#define NS_IW_IE_CIPHER_TKIP          2
+#define NS_IW_IE_CIPHER_WRAP          3
+#define NS_IW_IE_CIPHER_CCMP          4
+#define NS_IW_IE_CIPHER_WEP104        5
+
+/* Key management in GENIE */
+#define NS_IW_IE_KEY_MGMT_NONE        0
+#define NS_IW_IE_KEY_MGMT_802_1X      1
+#define NS_IW_IE_KEY_MGMT_PSK         2
 
 typedef struct {
 	char *essid;
 	char *apaddr;
 	int quality;
 	int en_method;
+	int pairwise;
+	int group;
+	int key_mgmt;
 	gboolean haskey;
 } ap_info;
 
