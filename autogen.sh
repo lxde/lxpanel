2e498977f285dbaed9fb1cd1097e4d31b091b1fc
Jürgen Hötzel <juergen@archlinux.org>
2009-06-13 09:51:47 +0000

    Automake 1.11 support

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
-AM_VERSION=-1.10
 AC_VERSION=
+
+AUTOMAKE=${AUTOMAKE:-automake}
+AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/ *([^()]*)$//' -e 's/.* \(.*\)/\1/' -e 's/-p[0-9]\+//')
+
+if [ "$AM_INSTALLED_VERSION" != "1.10" \
+    -a "$AM_INSTALLED_VERSION" != "1.11" ];then
+	echo
+	echo "You must have automake > 1.10 or 1.11 installed to compile lxpanel."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+	exit 1
+fi
 
 set -x
 
@@ -10,9 +21,9 @@ fi
 
 ${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
 ${AUTOHEADER:-autoheader$AC_VERSION} --force
-AUTOMAKE=${AUTOMAKE:-automake$AM_VERSION} libtoolize -c --automake --force
-AUTOMAKE=${AUTOMAKE:-automake$AM_VERSION} intltoolize -c --automake --force
-${AUTOMAKE:-automake$AM_VERSION} --add-missing --copy --include-deps
+$AUTOMAKE libtoolize -c --automake --force
+$AUTOMAKE intltoolize -c --automake --force
+$AUTOMAKE --add-missing --copy --include-deps
 ${AUTOCONF:-autoconf$AC_VERSION}
 
 # mkinstalldirs was not correctly installed in some cases.
