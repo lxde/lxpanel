4df357cd4cea0117b289f600171cc43ae5c3a4fe
Abel Fírvida Donéstevez <afirvida@users.sourceforge.net>
2009-07-15 13:28:39 +0000

    commiting include/lxpanel/plugin.h changes

--- include/lxpanel/plugin.h
+++ include/lxpanel/plugin.h
@@ -95,33 +95,55 @@ struct _Panel{
 
 /* Plugin */
 struct _Plugin;
+
+/* Support for external plugin versioning.
+ * Plugins must invoke PLUGINCLASS_VERSIONING when they instantiate PluginClass. */
+#define PLUGINCLASS_VERSION 1
+#define PLUGINCLASS_VERSIONING \
+    structure_size : sizeof(PluginClass), \
+    structure_version : PLUGINCLASS_VERSION
+
+/* Representative of an available plugin. */
 typedef struct {
-    char *fname;
-    int count;
-    GModule *gmodule;
 
-    int dynamic : 1;
-    int invisible : 1;
-    char *type;
-    char *name;
-    char *version;
-    char *description;
+    /* Keep these first.  Do not make unnecessary changes in structure layout. */
+    unsigned short structure_size;		/* Structure size, for versioning support */
+    unsigned short structure_version;		/* Structure version, for versioning support */
 
-    int (*constructor)(struct _Plugin *this, char **fp);
-    void (*destructor)(struct _Plugin *this);
-    void (*config)(struct _Plugin *this, GtkWindow* parent);
-    void (*save)(struct _Plugin *this, FILE* fp);
-    void (*orientation)(struct _Plugin *this);
+    char * fname;				/* Plugin file pathname */
+    int count;					/* Reference count */
+    GModule * gmodule;				/* Associated GModule structure */
+
+    int dynamic : 1;				/* True if dynamically loaded */
+    int unused_invisible : 1;			/* Unused; reserved bit */
+    int not_unloadable : 1;			/* Not unloadable due to GModule restriction */
+    int one_per_system : 1;			/* Special: only one possible per system, such as system tray */
+    int one_per_system_instantiated : 1;	/* True if one instance exists */
+    int expand_available : 1;			/* True if "stretch" option is available */
+    int expand_default : 1;			/* True if "stretch" option is default */
+
+    /* These fields point within the plugin image. */
+    char * type;				/* Internal name of plugin, to match external filename */
+    char * name;				/* Display name of plugin for selection UI */
+    char * version;				/* Version of plugin */
+    char * description;				/* Brief textual description of plugin for selection UI */
+
+    int (*constructor)(struct _Plugin * plugin, char ** fp);		/* Create an instance of the plugin */
+    void (*destructor)(struct _Plugin * plugin);			/* Destroy an instance of the plugin */
+    void (*config)(struct _Plugin * plugin, GtkWindow * parent);	/* Request the plugin to display its configuration dialog */
+    void (*save)(struct _Plugin * plugin, FILE * fp);			/* Request the plugin to save its configuration to a file */
+    void (*panel_configuration_changed)(struct _Plugin * plugin);	/* Request the plugin to do a full redraw after a panel configuration change */
 } PluginClass;
 
-typedef struct _Plugin{
-    PluginClass *class;
-    Panel        *panel;
-    GtkWidget    *pwid;
-    int           expand;
-    int           padding;
-    int           border;
-    gpointer      priv;
+/* Representative of a loaded and active plugin attached to a panel. */
+typedef struct _Plugin {
+    PluginClass * class;			/* Back pointer to PluginClass */
+    Panel * panel;				/* Back pointer to Panel */
+    GtkWidget * pwid;				/* Top level widget; plugin allocates, but plugin mechanism, not plugin itself, destroys this */
+    int expand;					/* Expand ("stretch") setting for container */
+    int padding;				/* Padding setting for container */
+    int border;					/* Border setting for container */
+    gpointer priv;				/* Private context for plugin; plugin frees this in its destructor */
 } Plugin;
 
 /* misc */
