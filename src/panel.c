d60c93723c5fe95270de610a0e860b276b614094
Michael Rawson <michaelrawson76@gmail.com>
2012-05-06 10:11:55 +0100

    Fixed about icon problems

--- src/panel.c
+++ src/panel.c
@@ -696,7 +696,8 @@ static void panel_popupmenu_about( GtkMenuItem* item, Panel* panel )
     panel_apply_icon(GTK_WINDOW(about));
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about), VERSION);
     gtk_about_dialog_set_name(GTK_ABOUT_DIALOG(about), _("LXPanel"));
-    gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about), gtk_icon_theme_load_icon(panel->icon_theme, "ns-bothrs", 48, 0, NULL));
+    if(gtk_icon_theme_has_icon(panel->icon_theme, "my-computer")) gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about), gtk_icon_theme_load_icon(panel->icon_theme, "my-computer", 48, 0, NULL));
+    else gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about), gdk_pixbuf_new_from_file(PACKAGE_DATA_DIR "/lxpanel/images/my-computer.png", NULL));
     gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about), _("Copyright (C) 2008-2011"));
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(about), _( "Desktop panel for LXDE project"));
     gtk_about_dialog_set_license(GTK_ABOUT_DIALOG(about), "This program is free software; you can redistribute it and/or\nmodify it under the terms of the GNU General Public License\nas published by the Free Software Foundation; either version 2\nof the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program; if not, write to the Free Software\nFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.");
@@ -709,7 +710,14 @@ static void panel_popupmenu_about( GtkMenuItem* item, Panel* panel )
 
 void panel_apply_icon( GtkWindow *w )
 {
-	GdkPixbuf* window_icon = gtk_icon_theme_load_icon(gtk_icon_theme_get_default(), "my-computer", 24, 0, NULL);
+	GdkPixbuf* window_icon;
+	
+	if(gtk_icon_theme_has_icon(gtk_icon_theme_get_default(), "my-computer")) {
+		window_icon = gtk_icon_theme_load_icon(gtk_icon_theme_get_default(), "my-computer", 24, 0, NULL);
+	}
+	else {
+		window_icon = gdk_pixbuf_new_from_file(PACKAGE_DATA_DIR "/lxpanel/images/my-computer.png", NULL);
+	}
     gtk_window_set_icon(w, window_icon);
 }
 
