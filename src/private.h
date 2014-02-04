aaaef69046a532b91d7be4ef27d5bb20b1779872
Andriy Grytsenko <andrej@rep.kiev.ua>
2014-02-04 04:23:49 +0200

    Add spawn_command_async() into private.h header.

--- src/private.h
+++ src/private.h
@@ -197,6 +197,9 @@ char* translate_exec_to_cmd( const char* exec, const char* icon,
 
 void show_error( GtkWindow* parent_win, const char* msg );
 
+gboolean spawn_command_async(GtkWindow *parent_window, gchar const* workdir,
+        gchar const* cmd);
+
 /* Parameters: const char* name, gpointer ret_value, GType type, ....NULL */
 GtkWidget* create_generic_config_dlg( const char* title, GtkWidget* parent,
                               GSourceFunc apply_func, Plugin * plugin,
