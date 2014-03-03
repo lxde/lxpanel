1a2e11737d23d297c2e316d3f7ac2715495f8809
Andriy Grytsenko <andrej@rep.kiev.ua>
2014-03-03 15:04:23 +0200

    Using HAVE_MENU_CACHE_DIR_LIST_CHILDREN requires #include <config.h>

--- src/menu-policy.c
+++ src/menu-policy.c
@@ -16,7 +16,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <menu-cache.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <glib.h>
 
 #include "menu-policy.h"
