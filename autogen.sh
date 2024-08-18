1f0e1aa018c5cbaa42bc30490685b1fa1d239fef
Ingo Brückl <ib@oddnet.de>
2024-08-18 17:04:00 +0200

    Revise autogen.sh
    It is now in line with the other LXDE programs.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,35 @@
 #! /bin/sh
-set -ex
+AC_VERSION=
+
+AUTOMAKE=${AUTOMAKE:-automake}
+AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
+
+# FIXME: we need a better way for version check later.
+case "$AM_INSTALLED_VERSION" in
+    1.1[1-9])
+	;;
+    *)
+	echo
+	echo "You must have automake >= 1.11 installed."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+	exit 1
+	;;
+esac
+
+
+if [ "x${ACLOCAL_DIR}" != "x" ]; then
+    ACLOCAL_ARG="-I ${ACLOCAL_DIR}"
+fi
 
 test -d m4 || mkdir m4
-autoreconf -i -f
-intltoolize -c --automake --force
-#xsltproc -o man/lxpanel.1 -nonet \
-#    http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl man/lxpanel.xml
-#xsltproc -o man/lxpanelctl.1 -nonet \
-#    http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl man/lxpanelctl.xml
+set -x
+
+${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
+${AUTOHEADER:-autoheader$AC_VERSION} --force
+AUTOMAKE=$AUTOMAKE libtoolize -c --automake --force
+AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force
+$AUTOMAKE --add-missing --copy --include-deps
+${AUTOCONF:-autoconf$AC_VERSION}
 
 rm -rf autom4te.cache
